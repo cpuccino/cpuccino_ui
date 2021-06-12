687255cd11ff25ed6fde4a817056f2b664ad4305
cpuccino <thegreatcappuccino@gmail.com>
2021-06-13 03:38:17 +1000

    feat: Updated CCTypography modifier and configuration keys

--- lib/components/atoms/typography/cc_typography.dart
+++ lib/components/atoms/typography/cc_typography.dart
@@ -8,6 +8,7 @@ class CCTypography extends StatelessWidget {
   final Color color;
 
   final bool showDivider;
+  final double? dividerWidth;
 
   final List<CCTypographyModifier> modifiers;
 
@@ -16,17 +17,18 @@ class CCTypography extends StatelessWidget {
     this.text = '',
     this.color = const Color(0xFF575757),
     this.showDivider = false,
+    this.dividerWidth,
     this.modifiers = const [],
   });
 
   double _getFontSize() {
-    if (modifiers.contains(CCTypographyModifier.t1)) return CCTypographyConfiguration.T1_FONT_SIZE;
-    if (modifiers.contains(CCTypographyModifier.t2)) return CCTypographyConfiguration.T2_FONT_SIZE;
-    if (modifiers.contains(CCTypographyModifier.t3)) return CCTypographyConfiguration.T3_FONT_SIZE;
-    if (modifiers.contains(CCTypographyModifier.t5)) return CCTypographyConfiguration.T5_FONT_SIZE;
-    if (modifiers.contains(CCTypographyModifier.t6)) return CCTypographyConfiguration.T6_FONT_SIZE;
+    if (modifiers.contains(CCTypographyModifier.t1)) return CCTypographyConfiguration.FONT_SIZE_T1;
+    if (modifiers.contains(CCTypographyModifier.t2)) return CCTypographyConfiguration.FONT_SIZE_T2;
+    if (modifiers.contains(CCTypographyModifier.t3)) return CCTypographyConfiguration.FONT_SIZE_T3;
+    if (modifiers.contains(CCTypographyModifier.t5)) return CCTypographyConfiguration.FONT_SIZE_T5;
+    if (modifiers.contains(CCTypographyModifier.t6)) return CCTypographyConfiguration.FONT_SIZE_T6;
 
-    return CCTypographyConfiguration.T4_FONT_SIZE;
+    return CCTypographyConfiguration.FONT_SIZE_T4;
   }
 
   Widget _buildText(double fontSize) {
@@ -45,15 +47,11 @@ class CCTypography extends StatelessWidget {
 
   Widget _buildDivider(double fontSize) {
     return Container(
-      margin: EdgeInsets.only(top: 3, bottom: 3),
-      alignment: Alignment.center,
-      child: Container(
-        width: double.infinity,
-        height: fontSize / 5,
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(5),
-        ),
+      width: dividerWidth,
+      height: fontSize / 5,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(5),
       ),
     );
   }
@@ -64,12 +62,13 @@ class CCTypography extends StatelessWidget {
 
     return Container(
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
               icon ?? Container(),
-              icon != null ? SizedBox(width: 10) : Container(),
+              icon != null ? SizedBox(width: 5) : Container(),
               _buildText(fontSize),
             ],
           ),
--- lib/components/atoms/typography/cc_typography_modifier.dart
+++ lib/components/atoms/typography/cc_typography_modifier.dart
@@ -4,21 +4,21 @@ import 'package:cpuccino_ui/components/atoms/typography/cc_typography.dart';
  * [CCTypographyModifier] contains style overloads for [CCTypography]
  */
 enum CCTypographyModifier {
-  /** [CCTypographyModifier.t1] text config fontSize = [CCTypographyConfiguration.T1_FONT_SIZE] */
+  /** [CCTypographyModifier.t1] text config fontSize = [CCTypographyConfiguration.FONT_SIZE_T1] */
   t1,
 
-  /** [CCTypographyModifier.t2] text config fontSize = [CCTypographyConfiguration.T2_FONT_SIZE] */
+  /** [CCTypographyModifier.t2] text config fontSize = [CCTypographyConfiguration.FONT_SIZE_T2] */
   t2,
 
-  /** [CCTypographyModifier.t3] text config fontSize = [CCTypographyConfiguration.T3_FONT_SIZE] */
+  /** [CCTypographyModifier.t3] text config fontSize = [CCTypographyConfiguration.FONT_SIZE_T3] */
   t3,
 
-  /** [CCTypographyModifier.t4] text config fontSize = [CCTypographyConfiguration.T4_FONT_SIZE] */
+  /** [CCTypographyModifier.t4] text config fontSize = [CCTypographyConfiguration.FONT_SIZE_T4] */
   t4,
 
-  /** [CCTypographyModifier.t5] text config fontSize = [CCTypographyConfiguration.T5_FONT_SIZE] */
+  /** [CCTypographyModifier.t5] text config fontSize = [CCTypographyConfiguration.FONT_SIZE_T5] */
   t5,
 
-  /** [CCTypographyModifier.t6] text config fontSize = [CCTypographyConfiguration.T6_FONT_SIZE] */
+  /** [CCTypographyModifier.t6] text config fontSize = [CCTypographyConfiguration.FONT_SIZE_T6] */
   t6,
 }
